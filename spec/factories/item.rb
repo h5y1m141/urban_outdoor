a9083be68b3996fb07d1ba0bdaae656a722948ac
HiroshiOyamada <h5y1m141@gmail.com>
2015-04-25 11:54:15 +0900

    Itemモデルに対応するFactoryGirl定義

---
+++ spec/factories/item.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :item do
+    sequence(:title) { |n| "title#{n}" }
+    sequence(:url) { |n| "http://www.example.com/item/#{n}" }
+    sequence(:description) {|n| "description#{n}"}
+
+    trait :on_sale do
+      discounted true
+      original_price 10000
+      discount_price 5000
+    end
+
+    trait :off_sale do
+      discounted false
+      original_price 10000
+    end
+  end
+end
