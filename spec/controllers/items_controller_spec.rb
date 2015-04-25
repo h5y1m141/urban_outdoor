49ec518168ae8a47224b8b9c75c54af88b17e348
HiroshiOyamada <h5y1m141@gmail.com>
2015-04-25 11:33:45 +0900

    ItemsControllerについてのspec作成

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,5 +1,31 @@
-require 'rails_helper'
+describe ItemsController do
+  describe "indexアクションについて" do
+    let(:request) { get :index }
+    it_renders_template(:index)
+  end
 
-RSpec.describe ItemsController, type: :controller do
+  describe "showアクションについて" do
+    let(:request) { get :show, id: 1 }
+    it_renders_template(:show)
+  end
 
+  describe "editアクションについて" do
+    let(:request) { get :edit, id: 1 }
+    it_renders_template(:edit)
+  end
+
+  describe "destroyアクションについて" do
+    let(:request) { delete :destroy, id: 1 }
+    it_returns_http_status(200)
+  end
+
+  describe "createアクションについて" do
+    let(:request) { post :create }
+    it_returns_http_status(200)
+  end
+
+  describe "updateアクションについて" do
+    let(:request) { put :update, id: 1 }
+    it_returns_http_status(200)
+  end
 end
