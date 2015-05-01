7ed279c160fb8c082e970c93e0665310cc935211
HiroshiOyamada <h5y1m141@gmail.com>
2015-05-01 18:30:32 +0900

    Userモデルに対してポリモーフィックアソシエーション使ってお気に入り登録できるように設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+  has_many :reviews, as: :resource, class_name: ItemReview.name
+  has_many :favorites, as: :resource, class_name: ItemFavorite.name
 end
