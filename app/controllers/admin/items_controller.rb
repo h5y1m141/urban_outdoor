962ae446b6352d9922835df4d0578c7fd9f3a9e1
HiroshiOyamada <h5y1m141@gmail.com>
2016-02-04 18:36:12 +0900

    編集ページへの画面遷移を追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,14 @@
 class Admin::ItemsController < AdminController
+  before_action :set_item, only: [:edit, :destroy, :update]
   def index
     @items = Item.includes(:pictures).order("updated_at DESC").all.page(params[:page])
+  end
+
+  def edit
+  end
+
+  private
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
