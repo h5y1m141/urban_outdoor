fd52c8661ac6fc417bef0dcb6f8baab83bd42495
HiroshiOyamada <h5y1m141@gmail.com>
2016-02-12 13:33:15 +0900

    見出しの文章を入力できる機能実装

---
+++ app/assets/javascripts/admin/directives/description.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('UrbanOutdoorApp')
+  .directive('description', function($compile) {
+    return {
+      restrict: 'E',
+      scope: {
+        description: '=data',
+        contents: '=contents'
+      },
+      link: function (scope, element, attrs) {
+        element.bind('click', function (e) {
+          var idSelector = e.target.id;
+          if(idSelector === 'subHead') {
+            scope.contents.push({
+              tag_name: 'sub_head',
+              element_data: scope.description
+            });
+          } else {
+            scope.contents.push({
+              tag_name: 'description',
+              element_data: scope.description
+            });
+          }
+          element.html('');
+
+          $compile(element.contents())(scope);
+        });
+        scope.$watch('description', function(html){
+          var subHeadButton,
+              descriptionButton;
+          if(html) {
+          subHeadButton = '<button id="subHead" class="btn-info">見出しとして登録</button>';
+          descriptionButton = '<button id="description" class="btn-info">説明文として登録</button>';            
+            element.html(html.replace(/[\n\r]/g, '<br />') + subHeadButton + descriptionButton);
+            $compile(element.contents())(scope);
+          }
+        });
+      }
+    };
+  });
