ffa70e95cad575852c30955500c76c7f4afd0e7b
HiroshiOyamada <h5y1m141@gmail.com>
2016-02-29 15:00:49 +0900

    管理画面でアイテム編集できる機能実装

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,10 +5,30 @@ class Admin::ItemsController < AdminController
   end
 
   def edit
+    if(@item.tags.count == 0)
+      @item.tags.build
+    else
+      @item
+    end
+  end
+
+  def update
+    tag_values = item_params[:tags_attributes]['0']['name'].split(",")
+    tags = tag_values.map{|tag_name| Tag.where(name: tag_name).first_or_create(name: tag_name) }
+    @item.tags = []
+    @item.tags.push(tags)
+    if @item.save
+      redirect_to admin_items_path, notice: '更新が完了しました'
+    else
+      redirect_to edit_admin_article(@item), notice: '更新できません'
+    end
   end
 
   private
   def set_item
     @item = Item.find(params[:id])
+  end
+  def item_params
+    params.require(:item).permit(:name, :url , :original_price, :discounted, :discount_price, :description, tags_attributes: [:id, :name])
   end
 end
