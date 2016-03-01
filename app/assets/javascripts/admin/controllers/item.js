b414d0ed1f065de910ebb58c11eed366839dba58
HiroshiOyamada <h5y1m141@gmail.com>
2016-03-01 17:56:11 +0900

    サムネイル一覧からメイン画像をピックアップできる機能実装

--- app/assets/javascripts/admin/controllers/item.js
+++ app/assets/javascripts/admin/controllers/item.js
@@ -1,5 +1,12 @@
 'use strict';
 
 angular.module('UrbanOutdoorApp')
-  .controller('ItemCtrl', function ($scope, $http, $timeout, Brand) {
-  });
+  .controller('ItemCtrl', ['$scope', function ($scope) {
+    $scope.init = function(pictures){
+      $scope.pictures = JSON.parse(pictures);
+      $scope.thumbnail = $scope.pictures[0].image.url;
+    };
+    $scope.selectThumbnail = function(index) {
+      $scope.thumbnail = $scope.pictures[index].image.url;
+    };
+  }]);
