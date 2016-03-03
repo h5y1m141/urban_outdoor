1507ce872abbdfdb420c9a994e73aec15cee1545
HiroshiOyamada <h5y1m141@gmail.com>
2016-03-03 13:27:10 +0900

    Controller spec実装

--- spec/controllers/admin/articles_controller_spec.rb
+++ spec/controllers/admin/articles_controller_spec.rb
@@ -1,15 +1,43 @@
 describe Admin::ArticlesController, type: :controller do
   let(:user) { create(:user) }
+  let(:articles ) do
+    create_list(:article, 5)
+  end
   before(:each) { login_user(user) }
 
-  describe "newアクションについて" do
+  describe 'indexアクションについて' do
+    let(:request_without_parameter) { get :index, q: {}  }
+    let(:request_with_parameter) { get :index, q: { title_eq: articles.first.title} }
+    before(:each) { articles }
+    context '検索パラメータが空白の場合' do
+      it '@searchに検索結果がセットされる' do
+        request_without_parameter
+        expect(assigns(:search).result.all.empty?).to eq false
+      end
+      it '検索結果が得られる' do
+        request_without_parameter
+        expect(assigns(:search).result).to eq articles
+      end
+    end
+    context '検索パラメータに条件を指定した場合' do
+      it '@searchに検索結果がセットされる' do
+        request_with_parameter
+        expect(assigns(:search).result.all.empty?).to eq false
+      end
+      it '検索結果が得られる' do
+        request_with_parameter
+        expect(assigns(:search).result).to eq [articles.first]
+      end
+    end
+  end
+  describe 'newアクションについて' do
     let(:request) { get :new }
     it_renders_template(:new)
   end
 
-  describe "createアクションについて" do
+  describe 'createアクションについて' do
     before(:each) do
-      request.env["HTTP_ACCEPT"] = 'application/json'
+      request.env['HTTP_ACCEPT'] = 'application/json'
     end
     describe '記事作成のタイトルのみ渡された場合' do
       let(:response) {post :create , { title: '記事コンテンツのタイトル' } }
