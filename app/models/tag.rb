303da504c02f8f8c6ea389a9dbf496e0d21f3218
HiroshiOyamada <h5y1m141@gmail.com>
2016-03-09 12:49:36 +0900

    TagSearchModuleを読み込むとクローラー実行時にエラーになるのでひとまずコメントアウト

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  include TagSearchModule
+  # include TagSearchModule
   has_many :item_tag_relays, dependent: :destroy
   has_many :items, through: :item_tag_relays
 end
